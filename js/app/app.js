ee6776695ad7c57bc74200912cd134222d92d2b6
Cesar Chen <cesar6134@gmail.com>
2014-12-09 15:25:38 +0800

    Added the instances of cdg

--- js/app/app.js
+++ js/app/app.js
@@ -1,5 +1,9 @@
 (function () {
   'use strict';
 
-  
+  var KaraokeRender = function (canvasId) {
+    this.canvas = document.getElementById(canvasId);
+    this.context = this.canvas.getContext('2d');
+  };
+
 }) ();
