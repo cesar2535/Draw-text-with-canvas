d7a2f0b6fd5b62e5c1d2525e1225eb3cd919c4c4
Cesar Chen <cesar6134@gmail.com>
2014-12-09 17:23:05 +0800

    Added the requestAnimationFrame function on window

--- js/app/karaoke_drawer.js
+++ js/app/karaoke_drawer.js
@@ -1,6 +1,14 @@
 (function () {
   'use strict';
 
+  window.requestAnimationFrame = (function (callback) {
+    return window.requestAnimationFrame || window.webkitRequestAnimationFrame || 
+      window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || 
+      function (callback) {
+        window.setTimeout(callback, 1000/ 60);
+      };
+  }) ();
+
   var KaraokeDrawer = function (canvas) {
     this.canvas = canvas;
     this.context = this.canvas.getContext('2d');
--- js/app/karaoke_player.js
+++ js/app/karaoke_player.js
@@ -2,13 +2,21 @@
   'use strict';
 
   var KaraokePlayer = function (canvasId, playerId) {
-    this.canvas = document.getElementById(canvasId);
-    this.player = document.getElementById(playerId);
+    this.canvas = undefined;
+    this.player = undefined;
     this.drawer = undefined;
+
+    this.initial(canvasId, playerId);
   };
 
-  KaraokePlayer.prototype.initial = function() {
+  KaraokePlayer.prototype.initial = function (canvasId, playerId) {
+    this.canvas = document.getElementById(canvasId);
+    this.player = document.getElementById(playerId);
     this.drawer = new KaraokeDrawer(this.canvas);
+  };
+
+  KaraokePlayer.prototype.initializeAudioEvents = function() {
+
   };
 
   window.KaraokePlayer = KaraokePlayer;
